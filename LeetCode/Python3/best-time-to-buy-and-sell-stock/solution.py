f4fd2f5f2a7de915bd59483fcc8bb7fb367d7215
Bhuvan R Nayak <115876248+bhuvan3535-boop@users.noreply.github.com>
2026-08-06 17:47:10 +0530

    Add python3 solution for best-time-to-buy-and-sell-stock - Runtime: 59

---
+++ LeetCode/Python3/best-time-to-buy-and-sell-stock/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        min_price = prices[0]
+        for i in range(len(prices)):
+            cost = prices[i] - min_price
+            profit = max(profit, cost)
+            min_price = min(min_price, prices[i])
+        return profit
