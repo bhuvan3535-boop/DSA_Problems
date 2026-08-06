4f78a50385d541db562ea5592edf8fc205e25a4a
Bhuvan R Nayak <115876248+bhuvan3535-boop@users.noreply.github.com>
2026-08-06 17:42:14 +0530

    Add python3 solution for sort-an-array - Runtime: 1071

---
+++ LeetCode/Python3/sort-an-array/solution.py
@@ -0,0 +1,39 @@
+class Solution:
+    def merge(self, nums, l, mid, r):
+        a = []
+        b = []
+        for i in range(l, mid+1):
+            a.append(nums[i])
+        for i in range(mid+1, r+1):
+            b.append(nums[i])
+        
+        i,j,k =0,0,l
+        while k<=r:
+            if i == len(a):
+                nums[k] = b[j]
+                k+=1
+                j+=1
+            elif j == len(b):
+                nums[k] = a[i]
+                k+=1
+                i+=1
+            elif a[i]<b[j]:
+                nums[k] = a[i]
+                k+=1
+                i+=1
+            else:
+                nums[k] = b[j]
+                k+=1
+                j+=1
+    def mergeSort(self, nums, l,r):
+        if l>=r: # base case
+            return
+
+        mid = (r+l)//2
+        self.mergeSort(nums, l, mid)
+        self.mergeSort(nums, mid+1, r)
+
+        self.merge(nums, l, mid, r)
+    def sortArray(self, nums: List[int]) -> List[int]:
+        self.mergeSort(nums, 0, len(nums)-1)
+        return nums
