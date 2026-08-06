bb0ef7fe70f9ee5362e9bf61a0081ca37938f8ab
Bhuvan R Nayak <115876248+bhuvan3535-boop@users.noreply.github.com>
2026-08-06 17:47:37 +0530

    Add python3 solution for remove-duplicates-from-sorted-array-ii - Runtime: 81

---
+++ LeetCode/Python3/remove-duplicates-from-sorted-array-ii/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n = len(nums)
+        if(n <= 2):
+            return n
+        j = 1
+        for i in range(2,n):
+            if(nums[i] != nums[j-1]):
+                j += 1
+                nums[j] = nums[i]
+        return j+1
+        
