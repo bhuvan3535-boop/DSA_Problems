27dee1230c9e746e2928c255d571b23321974aef
Bhuvan R Nayak <115876248+bhuvan3535-boop@users.noreply.github.com>
2026-08-06 17:43:56 +0530

    Add java solution for max-consecutive-ones - Runtime: 2

---
+++ LeetCode/Java/max-consecutive-ones/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int ans = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 1){
+                count++;
+            }if(nums[i] == 0 || i == nums.length-1){
+                ans = Math.max(ans, count);
+                count = 0;
+            }
+        }
+        return ans;
+    }
+}
